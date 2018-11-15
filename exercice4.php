eeec9f2cf27658e51f25ead6a063eb1ed0bf680e
yacinemsd <messaoudi.y76@gmail.com>
2018-11-15 12:05:39 +0100

    fin partie1

---
+++ exercice4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $string_name='yacine';
+        $int_age=6;
+        $float_poids=91.5;
+        $bool_masculin=true;
+        echo "$string_name a $int_age pèse $float_poids est de sexe $bool_masculin";
+        ?>
+    </body>
+</html>
---
+++ exercice8.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $addition=3+4;
+        $multiplication=5*20;
+        $division=45/5;
+        echo "le résulatat de l'addition est $addition <br>le résultat de la multiplication est $multiplication<br>le résulat de la division est $division";
+
+        
+       
+        ?>
+    </body>
+</html>
